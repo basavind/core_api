5e2118b11b44684aa4797c7682776708a0ea0248
Michael Doronin <warrior2031@gmail.com>
2016-07-27 18:00:22 +0300

    apis works via flask_sqlaclhemy_rest lib

--- core_api/app.py
+++ core_api/app.py
@@ -1,1 +1,18 @@
-from flask import app
+from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core_api.models import Material, Slice, Base
+from rest.endpoints import create_api, register_all_apis
+
+
+engine = create_engine('sqlite:////tmp/test.db')
+Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)()
+app = Flask(__name__)
+
+
+apis, schema = create_api(
+    Material, session
+)
+register_all_apis(app, schema, (apis,))
+app.run()
--- core_api/models.py
+++ core_api/models.py
@@ -6,19 +6,19 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 class IntPk:
-    id = Integer(primary_key=True)
+    id = Column(Integer, primary_key=True)
 
 class Material(Base, IntPk):
     __tablename__ = 'materials'
     file_path = Column(String)
     slices = relationship(
         'Slice',
-        order_by='Bullet.position',
+        order_by='Slice.position',
         collection_class=ordering_list('position'),
         backref='material'
     )
 
 class Slice(Base, IntPk):
     __tablename__ = 'slices'
-    material_id = Integer(ForeignKey('Material.id'))
+    material_id = Column(Integer, ForeignKey('materials.id'))
     position = Column(Integer)
